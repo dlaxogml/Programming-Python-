1825bcefde67aaf09a87c7516f0efe2330962612
taeeeh <s2018w37@e-mirim.hs.kr>
2019-09-20 15:30:14 +0900

    기초 예외 처리

---
+++ 2학기/VII. 예외 처리/예외처리2.py
@@ -0,0 +1,12 @@
+f = open("file.txt", "w")
+try:
+    f.write("Hello World!/n")
+    f.write(100)        #TypeError: write() argument must be str, not int
+except NameError:
+    print("이름 에러")
+except TypeError:
+    print("타입 예외 발생(100은 쓸 수 없음)")
+except:
+    print("모르는 에러")
+finally:
+    f.close()
